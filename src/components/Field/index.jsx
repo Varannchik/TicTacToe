ddd3ed77355dead9b22e3dbb55f4a7e3ed44b363
a.varakuta <a.varakuta@e-tender.ua>
2020-01-09 09:33:01 +0200

    50%

--- src/components/Field/index.jsx
+++ src/components/Field/index.jsx
@@ -13,26 +13,19 @@ export default class extends Component{
         }
     }
     
-    clickHandler = (id) => {             
+    clickHandler = (id) => {
         const newState = update(this.state, {count:{$set: id}});
-        console.log( "id: "+ id)  
-        console.log("cells: "+ this.state.cells);
-        console.log("count: "+ this.state.count);
-        
-        
+        this.setState(newState);
         let curSquare = this.state.cells;
-
-        if(curSquare[id] === null){        
-            curSquare[id] = (this.state.count % 2 === 0) ? "x" : "o"; //если четное пишем х ,если нет пишем о            
-            this.setState({count: this.state.count + 1});
-            this.setState({cells: curSquare});            
+        if(curSquare[id] === null){
+            curSquare[id] = (this.state.count % 2 === 0) ? "x" : "o"; //если четное пишем х ,если нет пишем о
+            this.setState({count: this.state.count+1});
+            this.setState({cells: curSquare});
         }
-        
-        //this.setState(newState);
     }
 
     checkX =()=>{
-        //return ({1,win},{0});
+        
     }
     checkY =()=>{
 
